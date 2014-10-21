bad8f299162257a5941e99fce5b0aa38bf83878d
U-bender\ediskrad <ediskrad@gmail.com>
2014-10-21 20:25:11 +0200

    Added row.name=FALSE to write.table

--- run_analysis.R
+++ run_analysis.R
@@ -71,4 +71,4 @@ melt_data = melt(data_set_red, id = id_labels, measure.vars = data_labels)
 
 tidy_data   = dcast(melt_data, Subject + Activity ~ variable, mean)
 
-write.table(tidy_data, file = "./tidy_data.txt")
+write.table(tidy_data, file = "./tidy_data.txt", row.name=FALSE)
